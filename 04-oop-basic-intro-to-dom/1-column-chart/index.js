e03bc1e1d1b35d1f9dce9a046d078e5ae48f88ae
Andrey Pokrovsky <iampokrovsky@gmail.com>
2021-10-04 22:14:39 +0300

    Добавляет компонент ColumnChart

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,95 @@
 export default class ColumnChart {
+  element;
+  updatableElements = {};
+  chartHeight = 50;
 
+  constructor({
+    data = [],
+    label = '',
+    link = '',
+    value = 0,
+    formatHeading = data => data
+  } = {}) {
+    this.data = data;
+    this.label = label;
+    this.link = link;
+    this.value = formatHeading(value);
+
+    this.render();
+    this.update(this.data);
+  }
+
+  render() {
+    const element = document.createElement("div");
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+    this.updatableElements = this.getUpdatableElements();
+  }
+
+  get template() {
+    return `
+      <div class="column-chart column-chart_loading" style="--chart-height: ${this.chartHeight}">
+        <div class="column-chart__title">
+          Total ${this.label}
+          <a class="column-chart__link" href="${this.link}">View all</a>
+          ${this.getLink()}
+        </div>
+        <div class="column-chart__container">
+          <div data-element="header" class="column-chart__header">${this.value}</div>
+          <div data-element="body" class="column-chart__chart"></div>
+        </div>
+      </div>
+    `;
+  }
+
+  getLink() {
+    return this.link ? '<a class="column-chart__link" href="${this.link}">View all</a>' : '';
+  }
+
+  update(data) {
+    if (data.length) {
+      this.element.classList.toggle('column-chart_loading', false);
+    } else {
+      this.element.classList.toggle('column-chart_loading', true);
+    }
+
+    this.updatableElements.body.innerHTML = this.getColumnBody(data);
+  }
+
+  getUpdatableElements() {
+    const result = {};
+    const elements = this.element.querySelectorAll('[data-element]');
+
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+
+      result[name] = subElement;
+    }
+
+    return result;
+  }
+
+  getColumnBody(data) {
+    const maxValue = Math.max(...data);
+    const scale = this.chartHeight / maxValue;
+
+    return data
+      .map(item => {
+        const percent = (item / maxValue * 100).toFixed(0);
+
+        return `<div style="--value: ${Math.floor(item * scale)}" data-tooltip="${percent}%"></div>`;
+      })
+      .join('');
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.updatableElements = {};
+
+  }
 }
